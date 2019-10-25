205f92a05f75af4867a04a79e507567b3dbaf6c4
Mateusz <15845263+maateo@users.noreply.github.com>
2019-10-25 17:39:32 +0100

    Added in code comments

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ from sklearn.metrics import confusion_matrix
 
 
 def get_training_and_evaluation_data():
+    # Read in the file
     movie_review_xlsx = pd.read_excel("movie_reviews.xlsx")
 
+    # Get reviews and sentiments where the data is marked as "train"
     reviews_training_data_list = movie_review_xlsx["Review"][movie_review_xlsx["Split"] == "train"]
     sentiment_training_data_list = movie_review_xlsx["Sentiment"][movie_review_xlsx["Split"] == "train"]
 
+    # Get reviews and sentiments where the data is marked as "train"
     reviews_testing_data_list = movie_review_xlsx["Review"][movie_review_xlsx["Split"] == "test"]
     sentiment_testing_data_list = movie_review_xlsx["Sentiment"][movie_review_xlsx["Split"] == "test"]
 
+    # Print out the gathered information
     print("There are: "
           "\n\t%d reviews used for training, where %d are positive, and %d are negative"
           "\n\t%d reviews used for testing, where %d are positive, and %d are negative"
@@ -27,11 +31,12 @@ def get_training_and_evaluation_data():
               sentiment_testing_data_list[sentiment_testing_data_list == "negative"].size
           ))
 
+    # Return the required data
     return reviews_training_data_list, sentiment_training_data_list, reviews_testing_data_list, sentiment_testing_data_list
 
 
 def prepare_and_convert_training_data_to_word_list(reviews_training_data_list, minimum_word_length, minimum_number_of_word_occurrence):
-    # This does the following:
+    # This method does the following:
     #  1) Keep only alphanumeric and spaces between words
     #  2) Make them lowercase
     #  3) Split so it looks like this:
@@ -39,12 +44,13 @@ def prepare_and_convert_training_data_to_word_list(reviews_training_data_list, m
     #           1 [its, a, pretty, good, cast, but, the, film, h...
     #           2 [this, ludicrous, film, offers, the, standard, ...
     #       ]
+    # Sanitise the data, make it lower case and finally split each row into words
     reviews_in_words = reviews_training_data_list.str \
         .replace("[^a-zA-Z ]", " ") \
         .str.lower() \
         .str.split()
 
-    words_occurrences_key_value = {}  # eg: "entertaining:1421"
+    words_occurrences_key_value = {}  # This will store data like this: "entertaining:1421"
     for words in reviews_in_words:
         # Get words from each review
         for word in words:
@@ -60,28 +66,30 @@ def prepare_and_convert_training_data_to_word_list(reviews_training_data_list, m
         if words_occurrences_key_value[word] >= minimum_number_of_word_occurrence:
             words_meeting_criteria.append(word)
 
+    # Return words above the minimum length and about the frequency
     return words_meeting_criteria
 
 
 def frequency_of_words_in_feature(words_to_look_for, review_dataset_to_search):
     """
-    TODO: update this statement:
     # Function that goes through all reviews in review_dataset_to_search and counts for each of these words the number of reviews the word appears in
 
     :param words_to_look_for: words that we are trying to find, and will be keeping a tally of whether they show up in a review or not
     :param review_dataset_to_search: the reviews to look through
-    :return:
+    :return: Frequency how many times the words show up in the reviews
     """
+    # Sanitise the data, make it lower case and finally split each row into words
     sanitised_review_dataset_to_search = review_dataset_to_search.str \
         .replace("[^a-zA-Z0-9 ]", " ") \
         .str.lower() \
         .str.split()
 
     counter = Counter(words_to_look_for)
-    for c in counter:
-        counter[c] = 0
+    for word in counter:
+        counter[word] = 0
 
     for index, review in sanitised_review_dataset_to_search.iteritems():
+        # Increment the counter for where the words match
         counter.update(set.intersection(set(review), words_to_look_for))
 
     return counter
@@ -91,15 +99,17 @@ def calculate_likelihood_using_laplace(frequency_of_positive_words, frequency_of
     # Each word extracted in test 2 -> binary feature of review, indicating that it's either present or absent
     # posterior 𝑃[𝜔𝑖 | x] is equal to likelihood 𝑃 [𝑥 | 𝜔i] and prior P[wi] divided by evidence P[x]
 
-    frequencies_dictionary = {}
+    frequencies_dictionary = {}  # Stores the frequencies of the words
     for word in frequency_of_positive_words:
+        # Go through each words, and apply laplace smoothing to them, using the len() of words as we need the total number distinct words
         likelihood_positive_laplace = np.divide(frequency_of_positive_words.get(word) + 1, total_number_of_positive_reviews + len(frequency_of_positive_words))
         likelihood_negative_laplace = np.divide(frequency_of_negative_words.get(word) + 1, total_number_of_negative_reviews + len(frequency_of_negative_words))
 
-        pos_neg_fraction = [likelihood_positive_laplace, likelihood_negative_laplace]
+        # This will let us store the two values as a list for each key. eg: "happy : [0.05421, 0.0020212]"
+        positive_negative_fractions_list = [likelihood_positive_laplace, likelihood_negative_laplace]
+        frequencies_dictionary[word] = positive_negative_fractions_list
 
-        frequencies_dictionary[word] = pos_neg_fraction
-
+    # Calculate the priors
     positive_priors = np.divide(total_number_of_positive_reviews, total_number_of_positive_reviews + total_number_of_negative_reviews)
     negative_priors = np.divide(total_number_of_negative_reviews, total_number_of_negative_reviews + total_number_of_positive_reviews)
 
@@ -129,12 +139,22 @@ def predict_sentiment_label(review_text, positive_prior, negative_prior, likelih
 
 
 def get_optimal_word_length_using_k_folds(reviews_training_data_list, sentiment_training_data_list, start_word_length_inclusive, end_word_length_inclusive):
-    average_run_results = []  # keeps average result for each min word length run
+    """
+    Using k folds, it gets the best minimum word length
+
+    :param reviews_training_data_list: Review data to train kfolds on
+    :param sentiment_training_data_list: Sentiment data to train kfolds on
+    :param start_word_length_inclusive: Minimum word length to start on
+    :param end_word_length_inclusive: Minimum word length to finish on
+    :return: optimal minimum word length
+    """
+    average_all_word_lengths_results = []  # keeps average result for each minimum word length run
     for current_min_word_length in range(start_word_length_inclusive, end_word_length_inclusive + 1):  # +1 to have it inclusive
-        this_run_results = []
+        current_word_length_results = []  # Keeps results for this word length
 
         kf = model_selection.KFold(n_splits=5, shuffle=True)
         for train_index, test_index in kf.split(reviews_training_data_list, sentiment_training_data_list):
+            # Get the correct data based on how the kfold decided to divide up the data
             reviews_training_train_fold_data = reviews_training_data_list.iloc[train_index]
             reviews_training_test_fold_data = reviews_training_data_list.iloc[test_index]
             sentiment_training_train_fold_data = sentiment_training_data_list.iloc[train_index]
@@ -157,35 +177,32 @@ def get_optimal_word_length_using_k_folds(reviews_training_data_list, sentiment_
                                                                                                                  frequency_of_words_in_negative_reviews,
                                                                                                                  total_positive_reviews,
                                                                                                                  total_negative_reviews)
-            print("likelihood_laplace_dictionary", likelihood_laplace_dictionary)
-            print("positive_priors", positive_priors)
-            print("negative_priors", negative_priors)
             print("Finished getting the likelihood using laplace")
 
-            predicted_sentiment_list = []
+            predicted_sentiment_list = []  # Used for building the list which is later fed into the confusion matrix
             for review in reviews_training_test_fold_data:
                 predicted_sentiment = predict_sentiment_label(review, positive_priors, negative_priors, likelihood_laplace_dictionary)
                 predicted_sentiment_list.append(predicted_sentiment)
 
             true_negative, false_positive, false_negative, true_positive = confusion_matrix(sentiment_training_test_fold_data, predicted_sentiment_list).ravel()
 
-            print("true negative", true_negative)
-            print("false positive", false_positive)
-            print("false negative", false_negative)
-            print("true positive", true_positive)
+            # Append the percentage at the end of each fold
+            current_word_length_results.append(np.divide(np.sum([true_positive, true_negative]), len(sentiment_training_test_fold_data)))
 
-            this_run_results.append(np.divide(np.sum([true_positive, true_negative]), len(sentiment_training_test_fold_data)))  # Append the percentage at the end of each fold
-
-        average_score_for_min_length = np.mean(this_run_results)
+        average_score_for_min_length = np.mean(current_word_length_results)  # Get average result for this word length
         print("For word length", current_min_word_length, "the average accuracy was", average_score_for_min_length, "%")
-        average_run_results.append(average_score_for_min_length)  # get average of each run
+        average_all_word_lengths_results.append(average_score_for_min_length)  # get average of each run
 
-    best_min_word_length = average_run_results.index(max(average_run_results)) + 1  # Getting best word length, +1 to account for list starting at 0
+    best_min_word_length = average_all_word_lengths_results.index(max(average_all_word_lengths_results)) + 1  # Getting best word length, +1 to account for list starting at 0
     print("Best average was achieved with minimum word length: ", best_min_word_length)
     return best_min_word_length
 
 
 def task_2_to_5(reviews_testing_data_list, reviews_training_data_list, sentiment_testing_data_list, sentiment_training_data_list):
+    """
+    Does everything from task 2 to 5, just to keep the main method clean :)
+    """
+
     print("Starting to convert training data into individual words")
     reviews_training_data_words_list = prepare_and_convert_training_data_to_word_list(reviews_training_data_list, 4, 100)
     print("Finished converting training data into individual words")
@@ -199,7 +216,7 @@ def task_2_to_5(reviews_testing_data_list, reviews_training_data_list, sentiment
     likelihood_laplace_dictionary, positive_priors, negative_priors = calculate_likelihood_using_laplace(frequency_of_words_in_positive_reviews, frequency_of_words_in_negative_reviews,
                                                                                                          total_positive_reviews, total_negative_reviews)
     print("Finished getting the likelihood using laplace")
-    predicted_sentiment_list = []
+    predicted_sentiment_list = []  # List of predicted sentiments which is later fed into the confusion matrix
     for review in reviews_testing_data_list:
         predicted_sentiment = predict_sentiment_label(review, positive_priors, negative_priors, likelihood_laplace_dictionary)
         predicted_sentiment_list.append(predicted_sentiment)
@@ -215,6 +232,9 @@ def main():
     reviews_training_data_list, sentiment_training_data_list, reviews_testing_data_list, sentiment_testing_data_list = get_training_and_evaluation_data()
     print("Finished fetching data from file")
 
+    print("##############################")
+    print("#####   Do task 2 to 5   #####")
+    print("##############################")
     task_2_to_5(reviews_testing_data_list, reviews_training_data_list, sentiment_testing_data_list, sentiment_training_data_list)
 
     print("##############################")
@@ -223,6 +243,7 @@ def main():
 
     optimal_word_length = get_optimal_word_length_using_k_folds(reviews_training_data_list, sentiment_training_data_list, 1, 10)
     print("Starting to convert training data into individual words")
+    # Get reviews above the optimal minimum word length
     reviews_training_data_words_list = prepare_and_convert_training_data_to_word_list(reviews_training_data_list, optimal_word_length, 100)
     print("Finished converting training data into individual words")
 
@@ -252,10 +273,14 @@ def main():
     accuracy = np.divide(np.sum([true_positive, true_negative]), len(sentiment_testing_data_list))
     print("Accuracy from optimal word length", optimal_word_length, "obtained from the k folds, is", accuracy, "%")
 
+    # Option to write your own reviews
+    print("##############################")
+    print("# Optional: Your own reviews #")
+    print("##############################")
     while True:
         review = input("Enter your review: ")
         predicted_sentiment = predict_sentiment_label(review, positive_priors, negative_priors, likelihood_laplace_dictionary)
-        print("Hocus Pocus, AbraKebabra, Beep Boop, I predict your review as:", predicted_sentiment)
+        print("Beep Boop, I predict your review as:", predicted_sentiment)
 
 
 main()
